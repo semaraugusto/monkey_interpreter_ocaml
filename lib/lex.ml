0f7181e17a6dd6db42d578f76431b607728b377e
semar <semaraugusto@gmail.com>
2023-07-30 23:02:42 -0300

    simple parsing is working

--- lib/lex.ml
+++ lib/lex.ml
@@ -8,12 +8,15 @@ module Lexer = struct
     mutable read_pos : int;
     mutable ch : char;
   }
-
+  
   let read_char l = 
+    print_endline ("old: \'" ^ (String.of_char l.ch) ^ "\'");
     if l.read_pos >= String.length l.input then
       l.ch <- '\000'
     else
       l.ch <- l.input.[l.read_pos];
+
+    print_endline ("new: \'" ^ (String.of_char l.ch) ^ "\'");
 
     (* Out_channel.output_char stdout l.ch; *)
     l.pos <- l.read_pos;
@@ -30,7 +33,67 @@ module Lexer = struct
     read_char l;
     l;;
 
+  let peek l = 
+    if l.read_pos >= String.length l.input then
+      '\000'
+    else
+      l.input.[l.read_pos];;
+
+  let rec read_identifier l output = 
+    let curr = l.ch in
+    let p = peek l in
+    print_endline ("identifier curr: \'" ^ (String.of_char curr) ^ "\'");
+    print_endline ("identifier peek: \'" ^ (String.of_char p) ^ "\'");
+    
+    match (curr, p) with 
+    |('a' .. 'z' | 'A' .. 'Z'), ('a' .. 'z' | 'A' .. 'Z') -> 
+      read_char l;
+      read_identifier l (curr :: output)
+    (* |('a' .. 'z' | 'A' .. 'Z'), ('(', ) ->  *)
+      (* String.of_char_list (List.rev (curr :: output));; *)
+    |('a' .. 'z' | 'A' .. 'Z'), _ -> 
+      String.of_char_list (List.rev (curr :: output))
+    | _ ->
+      String.of_char_list (List.rev output);;
+
+  let rec read_number l output = 
+    let curr = l.ch in
+    let p = peek l in
+    print_endline ("number curr: \'" ^ (String.of_char curr) ^ "\'");
+    print_endline ("number peek: \'" ^ (String.of_char p) ^ "\'");
+    (* match (curr, p) with  *)
+    match curr with 
+    |'0' .. '9' -> 
+    (* |'0' .. '9', '0' .. '9' ->  *)
+      (if Char.equal p ';' then 
+        String.of_char_list (List.rev (curr :: output))
+      else begin
+        read_char l;
+        read_number l (curr :: output)
+      end)
+    (* | '0' .. '9', (';') ->  *)
+        (* let out = String.of_char_list (List.rev (curr :: output)) in *)
+        (* print_endline ("number out: " ^ out); *)
+        (* out  *)
+    (* |'0' .. '9', (' ' | '\n') ->  *)
+    (*   read_char l; *)
+    (*   read_number l (curr :: output) *)
+      | _ ->
+      String.of_char_list (List.rev output);;
+
+    (* (String.of_char l.ch);; *)
+
+  let rec skip_whitespace l = 
+    match l.ch with 
+    | ' ' | '\n' -> 
+      read_char l;
+      skip_whitespace l
+    | _ -> ();;
+  
+
   let next_token l = 
+      skip_whitespace l;
+      let _old_ch = l.ch in
       let token = match l.ch with
     | '=' -> Token.newToken Token.Assign (Char.to_string l.ch)
     | '+' -> Token.newToken Token.Plus (Char.to_string l.ch)
@@ -40,9 +103,34 @@ module Lexer = struct
     | ')' -> Token.newToken Token.RParen (Char.to_string l.ch)
     | '{' -> Token.newToken Token.LBrace (Char.to_string l.ch)
     | '}' -> Token.newToken Token.RBrace (Char.to_string l.ch)
-    | _ -> Token.newToken Token.EOF "" in 
-    (* Token.print token;  *)
-    read_char l;
+    |'a' .. 'z' | 'A' .. 'Z' | '_' -> 
+        let literal = (read_identifier l []) in
+        (match literal with 
+          "fn" -> Token.newToken Token.Function literal
+          | "let" -> Token.newToken Token.Let literal
+          | _ -> Token.newToken Token.Ident literal)
+    |'0' .. '9' -> 
+        let number = (read_number l []) in
+        print_endline ("number: " ^ number);
+        
+        Token.newToken Token.Int number
+
+    | ' ' | '\n' -> 
+        failwith "space"
+      (* let _ = print_endline "space" in *)
+      (* read_char l; *)
+      (* next_token l; *)
+    | ch -> 
+        Printf.printf "ch: \'%c\'\n" ch;
+        Token.newToken Token.EOF "" in
+
+    print_string ("char: " ^ (String.of_char l.ch) ^ " -> ");
+    Token.print token; 
+    (* if (Char.equal old_ch l.ch) then *)
+      read_char l;
+    (* if not (Char.equal l.ch ';') then *)
+    (* print_string "next_token: "; *)
+    (* Token.print token; *)
     token;;
 
   let rec parse_input lexer result = 
--- test/monkey.ml
+++ test/monkey.ml
@@ -1,5 +1,5 @@
 open Monkey
-let _expected: Token.token list = [
+let expected_symbols: Token.token list = [
         {Token.t_type = Token.Assign; Token.literal = "="};
         {Token.t_type = Token.Plus; Token.literal = "+"};
         {Token.t_type = Token.LParen; Token.literal = "("};
@@ -11,39 +11,93 @@ let _expected: Token.token list = [
         {Token.t_type = Token.EOF; Token.literal = ""};
 ];;
 
+let expected_simple: Token.token list = [
+        {Token.t_type = Token.Let; Token.literal = "let"};
+        {Token.t_type = Token.Ident; Token.literal = "five"};
+        {Token.t_type = Token.Assign; Token.literal = "="};
+        {Token.t_type = Token.Int; Token.literal = "5"};
+        {Token.t_type = Token.Semicolon; Token.literal = ";"};
+        {Token.t_type = Token.Let; Token.literal = "let"};
+        {Token.t_type = Token.Ident; Token.literal = "ten"};
+        {Token.t_type = Token.Assign; Token.literal = "="};
+        {Token.t_type = Token.Int; Token.literal = "10"};
+        {Token.t_type = Token.Semicolon; Token.literal = ";"};
+        {Token.t_type = Token.Let; Token.literal = "let"};
+        {Token.t_type = Token.Ident; Token.literal = "add"};
+        {Token.t_type = Token.Assign; Token.literal = "="};
+        {Token.t_type = Token.Function; Token.literal = "fn"};
+        {Token.t_type = Token.LParen; Token.literal = "("};
+        {Token.t_type = Token.Ident; Token.literal = "x"};
+        {Token.t_type = Token.Comma; Token.literal = ","};
+        {Token.t_type = Token.Ident; Token.literal = "y"};
+        {Token.t_type = Token.RParen; Token.literal = ")"};
+        {Token.t_type = Token.LBrace; Token.literal = "{"};
+        {Token.t_type = Token.Ident; Token.literal = "x"};
+        {Token.t_type = Token.Plus; Token.literal = "+"};
+        {Token.t_type = Token.Ident; Token.literal = "y"};
+        {Token.t_type = Token.Semicolon; Token.literal = ";"};
+        {Token.t_type = Token.RBrace; Token.literal = "}"};
+        {Token.t_type = Token.Semicolon; Token.literal = ";"};
+        {Token.t_type = Token.Let; Token.literal = "let"};
+        {Token.t_type = Token.Ident; Token.literal = "result"};
+        {Token.t_type = Token.Assign; Token.literal = "="};
+        {Token.t_type = Token.Ident; Token.literal = "add"};
+        {Token.t_type = Token.LParen; Token.literal = "("};
+        {Token.t_type = Token.Ident; Token.literal = "five"};
+        {Token.t_type = Token.Comma; Token.literal = ","};
+        {Token.t_type = Token.Ident; Token.literal = "ten"};
+        {Token.t_type = Token.RParen; Token.literal = ")"};
+        {Token.t_type = Token.Semicolon; Token.literal = ";"};
+        {Token.t_type = Token.EOF; Token.literal = ""};
+];;
+
 let rec cmp lst1 lst2 =
 match (lst1,lst2) with
 | [], [] -> true
 | [],_ -> false
 | _,[] -> false
 | (h :: t), (hh:: tt) -> 
-        (* print_string "t1: "; *)
-        (* Token.print h; *)
-        (* print_string "t2: "; *)
-        (* Token.print hh; *)
+        Out_channel.output_string stdout "T1: ";
+        Token.print h;
+        Out_channel.output_string stdout "T2: ";
+        Token.print hh;
+        Out_channel.output_string stdout "\n";
         let is_equal = Token.eq h hh in 
-        (* assert_equal true (Token.eq h hh); *)
-        (* cmp t tt *)
         if is_equal then
-                (* print_endline "YAY"; *)
                 cmp t tt
-        else 
-                failwith "111";;
+        else begin
+                        (* Out_channel.output_string stdout "T1: "; *)
+                        (* Token.print h; *)
+                        (* Out_channel.output_string stdout "T2: "; *)
+                        (* Token.print hh; *)
+                        (* Out_channel.output_string stderr "\n"; *)
+                                (* " not equal T2: " ^ Token.print hh); *)
+                        (* Out_channel.output_string stderr ("T1: " ^ Token.print h ^ " not equal T2: " ^ Token.print hh); *)
+                false
+                        end
 
-let test_lexer () = 
+let test_symbols () = 
         let _lexer = Lexer.init "=+(){},;" in
         let _tokens = Lexer.parse_input _lexer [] in 
-        let is_equal = cmp _tokens _expected in
+        let is_equal = cmp _tokens expected_symbols in
         Alcotest.(check bool) "is_equal" true is_equal;;
 
-(* let test_lexer2 () =  *)
-(*         let _lexer = Lexer.init "=+(){},;" in *)
-(*         let _tokens = Lexer.parse_input _lexer [] in  *)
-(*         let is_equal = cmp _tokens _expected in *)
-(*         Alcotest.(check bool) "is_equal" true is_equal *)
+let test_simple () = 
+        let _lexer = Lexer.init "let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);" in
+        let _tokens = Lexer.parse_input _lexer [] in 
+        let is_equal = cmp _tokens expected_simple in
+        Alcotest.(check bool) "is_equal" true is_equal;;
 
 (* Run it *)
 let () =
   Alcotest.run "Symbols parsing" [
-    "symbols-parsing", [ Alcotest.test_case "symbols parsing" `Quick test_lexer ];
+    "symbols-parsing", [ Alcotest.test_case "symbols parsing" `Quick test_symbols ];
+    "parsing", [ Alcotest.test_case "simple parsing" `Quick test_simple ];
                 ]
